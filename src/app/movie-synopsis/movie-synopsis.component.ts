aaba7687ec2b1c7325bec6b596a5be79e620fdd7
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-08-16 19:04:15 -0400

    use MAT_DIALOG_DATA - @Inject into component

---
+++ src/app/movie-synopsis/movie-synopsis.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-movie-synopsis',
+  templateUrl: './movie-synopsis.component.html',
+  styleUrls: ['./movie-synopsis.component.scss']
+})
+
+export class MovieSynopsisComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string;
+      description: string;
+      poster: string;
+    }
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
