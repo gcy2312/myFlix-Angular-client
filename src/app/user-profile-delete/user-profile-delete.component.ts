15c569cc8c31b44e0e7aac3dbe20f3d8fcb06af6
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-08-17 18:28:28 -0400

    add function deleteUserAccount()

---
+++ src/app/user-profile-delete/user-profile-delete.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiDataService } from '../fetch-api-data.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-profile-delete',
+  templateUrl: './user-profile-delete.component.html',
+  styleUrls: ['./user-profile-delete.component.scss']
+})
+export class UserProfileDeleteComponent implements OnInit {
+
+  constructor(
+    public fetchApiData: ApiDataService,
+    public snackBar: MatSnackBar,
+    public router: Router,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  deleteUserAccount(): void {
+    this.fetchApiData.deleteUser().subscribe((resp: any) => {
+      this.snackBar.open(
+        'Your account has been deleted!', 'OK', {
+        duration: 2000,
+      }
+      );
+      localStorage.clear();
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000,
+      });
+      this.router.navigate(['/welcome']).then(() => {
+        window.location.reload();
+      });
+    }
+    );
+  }
+
+  cancel(): void {
+    this.router.navigate(['/profile']).then(() => {
+      window.location.reload();
+    });
+  }
+
+}
