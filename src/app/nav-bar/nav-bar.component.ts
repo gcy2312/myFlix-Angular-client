517408dc0e5b7f3eea66118c0f391c1e0b7d2129
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-08-17 18:27:24 -0400

    add function for logOut()

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+
+export class NavBarComponent implements OnInit {
+
+  constructor(
+    public router: Router,
+    public snackBar: MatSnackBar,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  logOut(): void {
+    localStorage.clear();
+    this.router.navigate(['/welcome']);
+    this.snackBar.open('You have successfully logged out!', 'OK', {
+      duration: 2000,
+    });
+  }
+
+}
