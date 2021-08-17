4241f87f4a7bf68beddcebf40ac8852927b0088b
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-08-17 18:29:16 -0400

    add updateUser() function

---
+++ src/app/user-profile-update/user-profile-update.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { ApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-profile-update',
+  templateUrl: './user-profile-update.component.html',
+  styleUrls: ['./user-profile-update.component.scss']
+})
+
+export class UserProfileUpdateComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  user: any = {};
+  movies: any = [];
+  userFavs: any = [];
+
+  constructor(
+    public fetchApiData: ApiDataService,
+    public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<UserProfileUpdateComponent>,
+
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  updateUser(): void {
+    this.fetchApiData.editUser(this.userData).subscribe((resp) => {
+      this.dialogRef.close(); //this will close modal on success
+      console.log(resp);
+      localStorage.setItem('user', resp.Username);
+      this.snackBar.open('Your profile has been successfully updated!', 'OK', {
+        duration: 2000,
+      });
+    }, (resp) => {
+      console.log(resp);
+      this.snackBar.open(resp, 'OK', {
+        duration: 2000,
+      });
+    });
+    // setTimeout(function () {
+    //   window.location.reload();
+    // }, 1000);
+  }
+
+}
